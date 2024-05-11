b789554274b617ee419418ea2e60d1e1c3e1c8a0
Dm&try <dmitrypanyukov@gmail.com>
2024-05-11 12:28:31 +0300

    Забыл ветку сделать.

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //( //int[] weights = new int[12];
-        // weights[0] = 90;
-        //int januaruWegth = weights[0];
-        //System.out.println(januaruWegth);
-        // System.out.println(weights[0]);
-        //System.out.println(weights[4]);
-        ////// int january = 0;
-//System.out.println(weights[january]);
-        ///int [] bigBox = {5, 6, 12};
-        //    System.out.println(bigBox[0]);
-        //  System.out.println(bigBox[1]);
-        //System.out.println(bigBox[2]);
-        //int[] weights = new int[] {12, 45, 57, 54, 87, 555, 756, 78, 0, 0, 62, 0};
-        //       int [] weights = {12, 45, 57, 54, 87, 555, 756, 78, 0, 0, 62, 0};
-        //for (int i = 0; i < 12; i++
-        //     for (int i = 0; i < weights.length; i++)
-        //   {
-        //     System.out.println(weights[i]);
-        //}
-        //System.out.println(bigBox[3]);
-        //System.out.println(weights[13]);
-        int [] arr = new int [10];
-        System.out.println(Arrays.toString(arr));
-        ///////////////////////////////
-        //////////////////////////////////
-        // )//
+
         System.out.println("Тема ДЗ Массив");
 
         byte dZ = 0;
@@ -75,11 +50,5 @@ public class Main {
         System.out.print(gre [2]+ ", ");
         System.out.print(gre [1]+ ", ");
         System.out.print(gre [0]+ ".");
-        /////////////
-
-        System.out.println();
-        System.out.println("Задача № " +(++dZ));
-        System.out.println();
-
     }
 }
