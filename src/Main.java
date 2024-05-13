13cd6ab4d28e06a44045438f08bbf029b7909875
Dm&try <dmitrypanyukov@gmail.com>
2024-05-13 16:35:57 +0300

    Сделал исправления

--- src/Main.java
+++ src/Main.java
@@ -14,41 +14,79 @@ public class Main {
         erm[0] = 1;
         erm[1] = 2;
         erm[2] = 3;
-        System.out.print(erm[0]+ ", ");
-        System.out.print(erm[1]+ ", " );
-        System.out.print(erm[2]);
-        System.out.print(".  " +erm[2]+ ", ");
-        System.out.print(erm[1]+ ", ");
-        System.out.print(erm[0]);
-        System.out.println();
-        for (int i = 0; i < 3; i++) {
-            System.out.println(i);
-            int b = erm [i];
-            if (b % 2 != 0) { erm [i] ++;
-            }
-        };
-        System.out.println(Arrays.toString(erm));
         ///////////////
         System.out.println();
         float [] flt = {1.57F, 7.654f, 9.986f};
-        System.out.print(flt[0]+ ", ");
-        System.out.print(flt[1]+ ", ");
-        System.out.print(flt[2]+ ".  ");
-        System.out.print(flt[2]+ ", ");
-        System.out.print(flt[1]+ ", ");
-        System.out.print(flt[0]);
         /////////////////
         System.out.println();
         int [] gre = new int[] {0, 1, 2, 3, 5};
-        System.out.print(gre [0]+ ", ");
-        System.out.print(gre [1]+ ", ");
-        System.out.print(gre [2]+ ", ");
-        System.out.print(gre [3]+ ", ");
-        System.out.print(gre [4]+ ".  ");
-        System.out.print(gre [4]+ ", ");
-        System.out.print(gre [3]+ ", ");
-        System.out.print(gre [2]+ ", ");
-        System.out.print(gre [1]+ ", ");
-        System.out.print(gre [0]+ ".");
+        ////////////////
+        System.out.println();
+        System.out.println("Задача № " +(++dZ));
+        System.out.println();
+        for (int i = 0; i < 3; ++i) {
+            System.out.print(erm[i]);
+            if (i < erm.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        /////////////////
+        System.out.println();
+        int i = flt.length;
+        for (float v : flt) {
+            System.out.print(v);
+            i--;
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        //////////////
+        System.out.println();
+        int a = gre.length;
+        for (int x : gre) {
+            System.out.print(x);
+            a--;
+            if (a > 0) {
+                System.out.print(", ");
+            }
+        }
+        ////////////////
+        System.out.println();
+        System.out.println();
+        System.out.println("Задача № " +(++dZ));
+        System.out.println();
+        for (int i1 = erm.length - 1; i1 >= 0; i1--) {
+            System.out.print(erm[i1]);
+            if (i1 > 0) {
+                System.out.print(", ");
+            }
+        }
+        //////////////////
+        System.out.println();
+        for (int i1 = flt.length - 1; i1 >= 0; i1--) {
+            System.out.print(flt[i1]);
+            if (i1 > 0) {
+                System.out.print(", ");
+            }
+        }
+        //////////////
+        System.out.println();
+        for (int i1 = gre.length - 1; i1 >= 0; i1--) {
+            System.out.print(gre[i1]);
+            if (i1 > 0) {
+                System.out.print(", ");
+            }
+        }
+        //////////
+        System.out.println();
+        System.out.println();
+        System.out.println("Задача № " +(++dZ));
+        System.out.println();
+        for (int s = 0; s < erm.length; s++) {
+            int b = erm [s];
+            if (b % 2 != 0) { erm [s] ++;
+            }
+        };
+        System.out.println(Arrays.toString(erm));
     }
 }
